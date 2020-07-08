8f3580f47c60f5c5e93802843db391f5a41c017e
Jess Daniel <jessdaniel474@gmail.com>
2020-07-08 19:44:54 -0400

    adds working repl parser

--- src/adv.py
+++ src/adv.py
@@ -10,14 +10,14 @@ room = {
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'grand overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow passage':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure chamber': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -27,12 +27,12 @@ earlier adventurers. The only exit is to the south."""),
 
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['foyer'].n_to = room['grand overlook']
+room['foyer'].e_to = room['narrow passage']
+room['grand overlook'].s_to = room['foyer']
+room['narrow passage'].w_to = room['foyer']
+room['narrow passage'].n_to = room['treasure chamber']
+room['treasure chamber'].s_to = room['narrow passage']
 
 #
 # Main
@@ -50,26 +50,41 @@ while is_playing is True:
 # * Prints the current room name
     print("Current Location: " + player.current_room)
 # * Prints the current description (the textwrap module might be useful here).
-    print("Description: " + room[player.current_room].description)
+    print("Description: " + room[player.current_room].description + "\n")
 # * Waits for user input and decides what to do.
     player_input = input("What is your next move? ")
 # If the user enters a cardinal direction, attempt to move to the room there.
-    if player_input == "n":
-        new_room = room[player.current_room].n_to
-        player.current_room = new_room.name.lower()
-    elif player_input == "s":
-        new_room = room[player.current_room].s_to
-        player.current_room = new_room.name.lower()
-    elif player_input == "e":
-        new_room = room[player.current_room].e_to
-        player.current_room = new_room.name.lower()
-    elif player_input == "w":
-        new_room = room[player.current_room].w_to
-        player.current_room = new_room.name.lower()
-    else:
-        print("Invalid movement")
+    def check_move(input):
+        if input == "n":
+            new_room = room[player.current_room].n_to
+            if new_room == None:
+                print("Invalid Movement")
+                return
+            player.current_room = new_room.name.lower()
+        elif input == "s":
+            new_room = room[player.current_room].s_to
+            if new_room == None:
+                print("Invalid Movement")
+                return
+            player.current_room = new_room.name.lower()
+        elif input == "e":
+            new_room = room[player.current_room].e_to
+            if new_room == None:
+                print("Invalid Movement")
+                return
+            player.current_room = new_room.name.lower()
+        elif input == "w":
+            new_room = room[player.current_room].w_to
+            if new_room == None:
+                print("Invalid Movement")
+                return
+            player.current_room = new_room.name.lower()
+        elif input == "q":
+            return
+        else:
+            print("Invalid movement")
 # Print an error message if the movement isn't allowed.
-#
+    check_move(player_input)
 # If the user enters "q", quit the game.
     if player_input == "q":
         is_playing = False
